977d81e79e9facfd61618d2d723c063b3bff5489
amadich <jeridi.moha@gmail.com>
2024-03-19 14:51:57 +0100

    framer-motion | whileInView ♣

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -15,11 +15,13 @@ const Section: React.FC = () => {
         const gridItemVariants = { hidden: {opacity: 0 , scale: 0} , visible: {opacity: 1 , scale: 1} };
 
    return (
-      <motion.section 
+      
+      <>
+         <motion.section 
             variants={gridSectionVariants}
             initial="hidden"
-            animate="visible"
-
+            whileInView="visible"
+            exit="hidden" // Add exit animation
             className="flex justify-center"
          >
          
@@ -35,8 +37,39 @@ const Section: React.FC = () => {
             <motion.div variants={gridItemVariants} className="bg-slate-300 shadow-md rounded-lg p-4 m-4 w-72 h-96">
                {/* Content for the fourth card */}
             </motion.div>
+
+
+            
         
       </motion.section>
+      <motion.section 
+            variants={gridSectionVariants}
+            initial="hidden"
+            whileInView="visible"
+            exit="hidden" // Add exit animation
+            className="flex justify-center"
+         >
+         
+            <motion.div variants={gridItemVariants} className="bg-slate-300 shadow-md rounded-lg p-4 m-4 w-72 h-96">
+               {/* Content for the first card */}
+            </motion.div>
+            <motion.div variants={gridItemVariants} className="bg-slate-300 shadow-md rounded-lg p-4 m-4 w-72 h-96">
+               {/* Content for the second card */}
+            </motion.div>
+            <motion.div variants={gridItemVariants} className="bg-slate-300 shadow-md rounded-lg p-4 m-4 w-72 h-96">
+               {/* Content for the third card */}
+            </motion.div >
+            <motion.div variants={gridItemVariants} className="bg-slate-300 shadow-md rounded-lg p-4 m-4 w-72 h-96">
+               {/* Content for the fourth card */}
+            </motion.div>
+
+
+            
+        
+      </motion.section>
+      </>
+
+      
    );
 };
 
